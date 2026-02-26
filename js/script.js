875dcbe6898f131396ea4f40b8f0c91c7930931d
Sergio Aguilar <isergioam@gmail.com>
2026-02-26 23:48:20 +0100

    HOME
    Rediseño de la página principal.

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Botón volver arriba
+const toTop = document.getElementById('toTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    toTop.style.display = 'block';
+  } else {
+    toTop.style.display = 'none';
+  }
+/*
+  // Animación fade-in
+  document.querySelectorAll('.fade-in').forEach(fader => {
+    const rect = fader.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 50) {
+      fader.classList.add('visible');
+    }
+  });*/
+});
+
+toTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+/* 🌙 MODO OSCURO */
+const toggle = document.getElementById('darkModeToggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  let imagen = document.getElementById("logo");
+  
+ /* if (imagen.src.includes("WOWGamerES_Logo.png")) {
+     imagen.src = "./images/WOWGamerES_Logo_white.png";
+   } else {
+     imagen.src = "./images/WOWGamerES_Logo.png";
+   }*/
+
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+	imagen.src = "./images/WOWGamerES_Logo_black.png";	
+  } else {
+    localStorage.setItem('theme', 'light');
+	imagen.src = "./images/WOWGamerES_Logo_white.png";
+  }
+});
+
+/* Guardar preferencia */
+window.addEventListener('DOMContentLoaded', () => {
+	let imagen = document.getElementById("logo");
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-mode');
+	imagen.src = "./images/WOWGamerES_Logo_black.png";
+  }
+});
+
+/* Menú móvil */
+
+document.querySelectorAll('.main-nav > ul > li > a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        const submenu = this.nextElementSibling;
+        if (submenu && submenu.classList.contains('submenu')) {
+            e.preventDefault(); // evitar cerrar página
+            submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+        }
+    });
+});
+
+/* Año actual en el Copyright */
+document.getElementById("year").textContent = new Date().getFullYear();
